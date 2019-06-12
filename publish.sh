eb77a4007483e5ab8d2fc47d3d5dec6ccc28e842
Vlad Holubiev <golubev.vld@gmail.com>
2019-06-12 14:06:23 +0300

    fix: use libreoffice layer name not git

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ us-east-1
 '
 
 for region in $REGIONS; do
-  aws lambda add-layer-version-permission --region $region --layer-name git \
+  aws lambda add-layer-version-permission --region $region --layer-name libreoffice \
     --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
     --version-number $(aws lambda publish-layer-version --region $region --layer-name libreoffice --zip-file fileb://layer.zip \
       --description "LibreOffice ${LO_VERSION} binary" --query Version --output text)
