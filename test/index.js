c4f2011cdcb2470f11f592e1950010d84a913ea3
Vlad Holubiev <golubev.vld@gmail.com>
2019-06-12 16:01:41 +0300

    test: refactor test to make them print logs

--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
 const {writeFileSync} = require('fs');
 const {execSync} = require('child_process');
 
+const convertCommand = `cd /tmp && /opt/instdir/program/soffice --headless --invisible --nodefault --view --nolockcheck --nologo --norestore --convert-to pdf --outdir /tmp test.txt`;
+
 exports.handler = () => {
   writeFileSync('/tmp/test.txt', Buffer.from('Hello World!'));
 
-  return execSync(
-    `cd /tmp && /opt/instdir/program/soffice --headless --invisible --nodefault --view --nolockcheck --nologo --norestore --convert-to pdf --outdir /tmp test.txt`
-  )
+  console.log(execSync(convertCommand).toString('utf8'));
+  console.log(execSync('ls -alh /tmp').toString('utf8'));
 };
