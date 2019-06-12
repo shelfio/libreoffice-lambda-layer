8cfa53920312a57b3cbe40b9df274a13e488da49
Vlad Holubiev <golubev.vld@gmail.com>
2019-06-12 12:24:57 +0300

    perf: publish only to us-east-1 for now

--- publish.sh
+++ publish.sh
@@ -3,21 +3,7 @@
 LO_VERSION=6.1.0.0.alpha0
 
 REGIONS='
-ap-northeast-1
-ap-northeast-2
-ap-south-1
-ap-southeast-1
-ap-southeast-2
-ca-central-1
-eu-central-1
-eu-west-1
-eu-west-2
-eu-west-3
-sa-east-1
 us-east-1
-us-east-2
-us-west-1
-us-west-2
 '
 
 for region in $REGIONS; do
