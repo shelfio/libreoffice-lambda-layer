8c7f7a79c48f1861807ae518665fe08056467d33
Vlad Holubiev <golubev.vld@gmail.com>
2019-06-12 16:18:33 +0300

    fix: don't publish to other regions

--- publish.sh
+++ publish.sh
@@ -2,22 +2,9 @@
 
 LO_VERSION=6.1.0.0.alpha0
 
+# It's 1 region for now since S3 bucket is there
 REGIONS='
-ap-northeast-1
-ap-northeast-2
-ap-south-1
-ap-southeast-1
-ap-southeast-2
-ca-central-1
-eu-central-1
-eu-west-1
-eu-west-2
-eu-west-3
-sa-east-1
 us-east-1
-us-east-2
-us-west-1
-us-west-2
 '
 
 aws s3 cp ./layer.zip s3://shelf-assets/libreoffice-lambda-layer/layer.zip
