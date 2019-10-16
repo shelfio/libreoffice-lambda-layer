d48d2e2b97376e8da67a8eca58137d6e8160b6d4
Vlad Holubiev <golubev.vld@gmail.com>
2019-10-16 12:34:51 +0300

    feat: publish in 3 new regions

--- publish.sh
+++ publish.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 
 LO_VERSION=6.1.0.0.alpha0
+LAYER_NAME='libreoffice'
 
-REGIONS='
-us-east-1
-us-east-2
-eu-west-1
-eu-central-1
-us-west-2
-ap-southeast-2
-'
+aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-"$TARGET_REGION"/layer.zip
 
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-us-east-1/layer.zip
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-us-east-2/layer.zip
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-eu-west-1/layer.zip
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-eu-central-1/layer.zip
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-us-west-2/layer.zip
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-ap-southeast-2/layer.zip
-
-for region in $REGIONS; do
-  aws lambda add-layer-version-permission --region $region --layer-name libreoffice \
-    --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
-    --version-number $(aws lambda publish-layer-version --region $region --layer-name libreoffice \
-      --description "LibreOffice ${LO_VERSION} binary" --query Version --output text \
-      --content S3Bucket=shelf-libreoffice-lambda-layer-$region,S3Key=layer.zip)
-done
+aws lambda add-layer-version-permission \
+  --region "$TARGET_REGION" \
+  --layer-name "$LAYER_NAME" \
+  --statement-id sid1 \
+  --action lambda:GetLayerVersion \
+  --principal '*' \
+  --version-number "$(aws lambda publish-layer-version \
+    --region "$TARGET_REGION" \
+    --layer-name libreoffice \
+    --description "LibreOffice ${LO_VERSION} binary" \
+    --query Version \
+    --output text \
+    --content S3Bucket=shelf-libreoffice-lambda-layer-"$TARGET_REGION",S3Key=layer.zip
+    )"
