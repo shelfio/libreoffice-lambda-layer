1f246cbcb668cc0b11493fe286d4edd0a7f4f411
Vlad Holubiev <golubev.vld@gmail.com>
2020-01-10 16:56:39 +0200

    feat: publish 2 versions: brotli + gzip

--- publish.sh
+++ publish.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 LO_VERSION=6.4.0.1
-LAYER_NAME='libreoffice'
 
-aws s3 cp ./layer.zip s3://shelf-libreoffice-lambda-layer-"$TARGET_REGION"/layer.zip
+aws s3 cp ./"$LAYER_FILENAME" s3://shelf-libreoffice-lambda-layer-"$TARGET_REGION"/"$LAYER_FILENAME"
 
 aws lambda add-layer-version-permission \
   --region "$TARGET_REGION" \
@@ -13,9 +12,9 @@ aws lambda add-layer-version-permission \
   --principal '*' \
   --version-number "$(aws lambda publish-layer-version \
     --region "$TARGET_REGION" \
-    --layer-name libreoffice \
-    --description "LibreOffice ${LO_VERSION} binary" \
+    --layer-name "$LAYER_NAME" \
+    --description "${LAYER_NAME} ${LO_VERSION} binary" \
     --query Version \
     --output text \
-    --content S3Bucket=shelf-libreoffice-lambda-layer-"$TARGET_REGION",S3Key=layer.zip
+    --content S3Bucket=shelf-libreoffice-lambda-layer-"$TARGET_REGION",S3Key="$LAYER_FILENAME"
     )"
